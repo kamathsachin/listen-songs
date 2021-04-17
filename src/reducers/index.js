0b8a7ae5ca924d1085c6e515c19ef7b60411e8d8
Sachin Kamath <kamathsachin@hotmail.com>
2021-04-17 03:55:47 -0500

    Combined the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songReducer = () => {
   return [
     { title: "No Scrubs", duration: "4.05" },
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer,
+});
